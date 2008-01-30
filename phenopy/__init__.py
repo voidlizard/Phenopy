931cb700ebe33a5d65f91da82e4a7b3dd967769b
Dmitry Zuikov <dmz@cobalt>
2008-01-30 20:30:33 +0300

    POST/multiple values fixed

--- phenopy/__init__.py
+++ phenopy/__init__.py
@@ -1,7 +1,7 @@
 """
     Misc stuffs
 """
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 __author__  = 'Dmitry Zuikov <dmz@phenopy.net>'
 __license__ = 'LGPL License'
 
--- phenopy/webutils/forms.py
+++ phenopy/webutils/forms.py
@@ -3,7 +3,7 @@ import re
 #TODO: to utilities
 class Form_Data(object):
 
-    def __init__(self, dict={}, transforms={}, **kw):
+    def __init__(self, obj={}, transforms={}, **kw):
         checkboxes = {}
         chb = re.compile(r'(\w+):(\w+)')
 
@@ -17,17 +17,26 @@ class Form_Data(object):
                 t = lambda x:x
             return t(val)
 
-        for k,v in dict.items():
-            m = chb.match(k)
-            if m:
-                name = m.group(1)
-                val = m.group(2)
-                if not checkboxes.has_key(name):
-                    checkboxes[name] = set()
-                checkboxes[name].add(val)
-            else:
-                setattr(self, k, tr(k,v))
+        lists = dict()
+        for k,v in obj.lists():
+            lists[k] = v
+
+        for k,v in obj.items():
+            if not lists.has_key(k):
+                m = chb.match(k)
+                if m:
+                    name = m.group(1)
+                    val = m.group(2)
+                    if not checkboxes.has_key(name):
+                        checkboxes[name] = set()
+                    checkboxes[name].add(val)
+                else:
+                    setattr(self, k, tr(k,v))
+
         for k,v in checkboxes.items():
+            setattr(self, k, tr(k,v))
+
+        for k,v in lists.items():
             setattr(self, k, tr(k,v))
 
 class Form_Errors(object):
